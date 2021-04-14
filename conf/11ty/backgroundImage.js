dd3330be10dbd482f88244add3588f3049426d52
andreaskundig <andreas.kuendig@gmx.ch>
2021-04-14 17:51:52 +0200

    adds backgroundImage shortcode (#5)

---
+++ conf/11ty/backgroundImage.js
@@ -0,0 +1,5 @@
+const backgroundImage = (imageUrl) =>
+     imageUrl &&
+      `background: url(${imageUrl}) no-repeat center center fixed; background-size: cover`;
+
+module.exports = { backgroundImage };
