8eed88bfdd72aa7a571163f26d216e4a4d0c3678
andreaskundig <andreas.kuendig@gmx.ch>
2021-04-22 17:46:16 +0200

    adapts internalLink shortcode to anchors (#5)

--- conf/11ty/internalLink.js
+++ conf/11ty/internalLink.js
@@ -2,11 +2,17 @@
 
 function internalLink(content, page, destination, destinationLocale) {
     let href = destination;
+    const hasAnchor = href.match(/#[^/]*$/);
     if(destinationLocale){
-        href = `/${destinationLocale}/${destination}/`;
+        href = `/${destinationLocale}/${destination}`;
+        if(!hasAnchor){
+            href += '/';
+        }
     }
-    const ariaCurrent =
-          page.url == href ? ' aria-current="page"' : '';
+    const hrefWithoutAnchor =
+          href.match(/(^\/.*?[^/]\/)(#\w+)?$/)[1] ;
+    const isCurrent = page.url === hrefWithoutAnchor;
+    const ariaCurrent = isCurrent ? ' aria-current="page"' : '';
     return `<a href="${href}"${ariaCurrent}>${content}</a>`;
 }
 
